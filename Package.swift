f6d26d95dcf0fc00001f1e611d84438a5315ca07
TikhonP <tikhon.petrishchev@gmail.com>
2024-09-22 17:33:26 +0300

    Reverted supported versions for non-iOS platforms

--- Package.swift
+++ Package.swift
@@ -6,9 +6,9 @@ let package = Package(
     name: "Fit",
     platforms: [
         .iOS(.v15),
-        .macOS(.v12),
-        .tvOS(.v15),
-        .watchOS(.v8)
+        .macOS(.v13),
+        .tvOS(.v16),
+        .watchOS(.v9)
     ],
     products: [
         .library(
